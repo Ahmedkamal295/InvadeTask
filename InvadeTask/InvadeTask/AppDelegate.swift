8ce6e54033012d1edd8c0aa75b3eab64a3531e8c
Ahmedkamal <Ahmedkamal121995@gmail.com>
2024-05-01 20:47:36 +0300

    create task

---
+++ InvadeTask/InvadeTask/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  InvadeTask
+//
+//  Created by Ahmed Kamal on 01/05/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    //MARK: - window
+    var window: UIWindow?
+    
+    //MARK: - didFinish
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ InvadeTask/Pods/Target Support Files/Pods-InvadeTask/Pods-InvadeTask-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InvadeTask : NSObject
+@end
+@implementation PodsDummy_Pods_InvadeTask
+@end
